567ba03b37af2ce450e40c2f98d125348eb16b87
Aarthi.S <168005532+Aarthi-2004@users.noreply.github.com>
2025-06-23 21:33:46 +0530

    Shuffle_An_Array.java

--- Shuffle_An_Array.java
+++ Shuffle_An_Array.java
@@ -1,3 +1,15 @@
+//Given an integer array nums, design an algorithm to randomly shuffle the array. All permutations of the array should be equally likely as a result of the shuffling.
+//Implement the Solution class:
+//Solution(int[] nums) Initializes the object with the integer array nums.
+//int[] reset() Resets the array to its original configuration and returns it.
+//int[] shuffle() Returns a random shuffling of the array.
+
+//Example 1:
+//Input
+//["Solution", "shuffle", "reset", "shuffle"]
+//[[[1, 2, 3]], [], [], []]
+//Output
+//[null, [3, 1, 2], [1, 2, 3], [1, 3, 2]]
 
 class Solution {
   public Solution(int[] nums) {
@@ -9,11 +21,11 @@ class Solution {
 
     public int[] shuffle() {
     int[] arr = nums.clone();
-…     private void swap(int[] arr, int i, int j) {
+    private void swap(int[] arr, int i, int j) {
      final int temp = arr[i];
      arr[i] = arr[j];
       arr[j] = temp;
-     }
     }
+  }
 
     
